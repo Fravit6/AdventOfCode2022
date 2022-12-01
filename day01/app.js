dd779f46455797f65284aeaa62dcad4f980c71ed
Francesco Vitale <fra.vit6@gmail.com>
2022-12-01 10:25:09 +0100

    metodo naive day1

--- day01/app.js
+++ day01/app.js
@@ -1,11 +1,47 @@
 console.clear()
 var fs = require('fs')
+const path = require('path')
 
-console.log('------------------------------------------')
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'))
+const demo = fs.readFileSync(path.join(__dirname, 'demo.txt'))
+// console.log(demo.toString())
 
-// fs.readFile('input.txt', 'utf8', function (err, data) {
-fs.readFile('demo.txt', 'utf8', function (err, data) {
-  if (err) throw err
+const array = demo.toString().split(/\r?\n/)
+array.push('')
 
-  var lines = data.split(/\r?\n/)
-})
+let max = 0
+let window = 0
+for (const elem of array) {
+  if (elem != '') window += +elem
+  else {
+    if (window >= max) max = +window
+    window = 0
+  }
+}
+console.log('Soluzione 1: ', max)
+
+// Part 2
+let somme = []
+window = 0
+max = [0, 0, 0]
+for (const elem of array) {
+  if (elem != '') window += +elem
+  else {
+    somme.push(window)
+    window = 0
+  }
+}
+//console.log(somme)
+
+for (const somma of somme) {
+  const minimo = max.find((e) => e <= somma)
+  if (somma >= minimo) {
+    max.splice(max.indexOf(minimo), 0, somma)
+    max.pop()
+  }
+}
+
+console.log(
+  'Soluzione 2: ',
+  max.reduce((a, b) => a + b, 0)
+)
