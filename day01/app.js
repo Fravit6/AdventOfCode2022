1e3e00140e5ec1b22be653b339d7474bbbb3e4e2
Francesco Vitale <fra.vit6@gmail.com>
2022-12-01 11:03:49 +0100

    minchange

--- day01/app.js
+++ day01/app.js
@@ -10,7 +10,6 @@ array.push('')
 
 // Part 1
 let max = 0
-let window = 0
 array.reduce((acc, elem) => {
   if (elem) return acc + +elem
   else max = Math.max(max, acc)
@@ -21,7 +20,7 @@ console.log('Soluzione 1: ', max)
 
 // Part 2
 let somme = []
-window = 0
+let window = 0
 max = [0, 0, 0]
 for (const elem of array) {
   if (elem != '') window += +elem
